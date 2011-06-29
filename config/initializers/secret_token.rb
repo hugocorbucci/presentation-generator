431d4b06023fd913d29b46d2f0a44282d7961ed0
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-06-29 11:12:17 -0300

    Initial commit to the presentation generator app. Hugo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PresentationGenerator::Application.config.secret_token = '57b95e9497c0ea5098ada872b1253f3d96a6306cebc76fa709b9c6a5c7b684b6b74274d6c175f739cdc405ee47778ad8253c8e17f2387be91683266bd0dd1398'
