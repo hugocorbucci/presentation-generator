8b031b30319e4e821394ac1e3c1fbce32c610732
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-07-03 20:18:33 -0300

    Starting the ajax presentation preview on start. A few fixmes related to JS differences between browsers. Since I had no Internet when doing this, examples also mock the ajax to a local file. Hugo

--- spec/controllers/presentations_controller_spec.rb
+++ spec/controllers/presentations_controller_spec.rb
@@ -2,39 +2,44 @@ require 'spec_helper'
 require 'lib/image_searcher'
 
 describe PresentationsController do
-  it "new action should have new presentation assigned" do
-    get :new
-    assigns(:presentation).should_not be_nil
+  context "new action" do
+    it "new action should have new presentation assigned" do
+      get :new
+      assigns(:presentation).should_not be_nil
+    end
+
+    it "new action should render new template" do
+      get :new
+      response.should render_template(:new)
+    end
   end
   
-  it "new action should render new template" do
-    get :new
-    response.should render_template(:new)
-  end
+  context "existing action" do
+    before do
+      @searcher = mock(ImageSearcher)
+      @searcher.should_receive(:get_image).and_return("http://www.image.com/image.png")
+      ImageSearcher.should_receive(:engine).and_return(@searcher)
+      
+      @presentation = Presentation.new(:content => "silver bullet")
+    end
+    
+    it "create action should redirect to show" do
+      Presentation.should_receive(:new).with("content" => "silver bullet").and_return(@presentation)
+      post :create, :presentation  => { :content => "silver bullet" }
+      response.should redirect_to(presentation_path(@presentation))
+    end
   
-  it "create action should redirect to show" do
-    searcher = mock(ImageSearcher)
-    searcher.should_receive(:get_image).and_return("http://www.image.com/image.png")
-    ImageSearcher.should_receive(:engine).and_return(searcher)
-    presentation = Presentation.new(:content => "silver bullet")
-    Presentation.should_receive(:new).and_return(presentation)
-    post :create, :presentation  => { :content => "silver bullet" }
-    response.should redirect_to(presentation_path(presentation))
-  end
+    it "show action should have presentation with id assigned" do
+      Presentation.should_receive(:find).with(1).and_return(@presentation)
+      get :show, :id  => 1
+      assigns(:presentation).should_not be_nil
+      assigns(:presentation).slides.should_not be_nil
+    end
   
-  it "show action should have presentation with id assigned" do
-    presentation = Presentation.new(:content => "silver bullet")
-    presentation.stub(:get_image).and_return("http://image.com/image.png")
-    Presentation.should_receive(:find).with(1).and_return(presentation)
-    get :show, :id  => 1
-    assigns(:presentation).should_not be_nil
-    assigns(:presentation).slides.should_not be_nil
-  end
-  
-  it "show action should render show template" do
-    presentation = Presentation.new(:content => "silver bullet")
-    Presentation.should_receive(:find).with(1).and_return(presentation)
-    get :show, :id => 1
-    response.should render_template(:show)
+    it "show action should render show template" do
+      Presentation.should_receive(:find).with(1).and_return(@presentation)
+      get :show, :id => 1
+      response.should render_template(:show)
+    end
   end
 end
