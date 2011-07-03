e3405230c961ad923b89d745e92c38d9f298f350
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-07-03 07:46:24 -0300

    Refactoring image searcher. Improving navigation. Formatting style. Hugo

--- lib/image_searcher.rb
+++ lib/image_searcher.rb
@@ -4,13 +4,14 @@ require 'uri'
 require 'json'
 
 class ImageSearcher
-
   def self.engine(engine)
-    engine = GoogleImageSearcher.new  if engine == "google" 
-
-    engine = FlickrImageSearcher.new if engine == "flickr"
- 
-    engine
+    engine == "google" ? GoogleImageSearcher.new : FlickrImageSearcher.new
+  end
+  
+  def get_image(words)
+    escaped_words = CGI::escape(words)
+    data = json_from(url(escaped_words))
+    parse(data)
   end
 
   protected
@@ -18,30 +19,36 @@ class ImageSearcher
       resource = Net::HTTP.get_response(URI.parse(url))
       JSON.parse(resource.body)
     end
-
 end
 
 class FlickrImageSearcher < ImageSearcher
-
-  def get_image(words)
-    escaped_words = CGI::escape(words)
+  def url(escaped_words)
     #TODO use api_key on config/flickr.yml
-    url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5170099ef3339caa3f610c7be46723c4&page=1&per_page=1&tags=#{escaped_words}&format=json&nojsoncallback=1"
-    response = json_from(url)
-    data = response["photos"]
-    photo = data["photo"][0]
-    "http://farm#{photo['farm']}.static.flickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_z.jpg"
+    "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5170099ef3339caa3f610c7be46723c4&page=1&per_page=1&tags=#{escaped_words}&format=json&nojsoncallback=1"
+  end
+  
+  def parse(data)
+    photos = data["photos"]
+    if photos.empty?
+      ""
+    else
+      photo = photos["photo"][0]
+      "http://farm#{photo['farm']}.static.flickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_z.jpg"
+    end
   end
 end
 
 class GoogleImageSearcher < ImageSearcher
-
-   def get_image(words)
-      escaped_words = CGI::escape(words)
-      url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{escaped_words}"
-      data = json_from(url)["responseData"]
-      results = data["results"]
+  def url(escaped_words)
+    "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{escaped_words}"
+  end
+  def parse(data)
+    results = data["responseData"]["results"]
+    if results.empty?
+      ""
+    else
       result = results[0]
       result["url"]
-   end
+    end
+  end
 end
