ba1f153e1f5aae365a19247596b0fcaf1c3f5ce5
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-07-05 00:38:24 -0300

    Adding support to JS preview on the new page. Hugo

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -5,8 +5,15 @@ class PresentationsController < ApplicationController
   
   def create
     presentation = Presentation.new(params[:presentation])
-    presentation.save
-    redirect_to presentation_path(presentation)
+    respond_to do |format|
+      format.html {
+        presentation.save        
+        redirect_to presentation_path(presentation)
+      }
+      format.js {
+        render :inline => presentation.slides.map{|slide| [slide.content, slide.image]}.to_json
+      }
+    end
   end
   
   def show
