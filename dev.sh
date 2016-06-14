0a4cd38dcc1ab737bb27a39f1551c3c6e303a1d6
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-06-14 14:34:06 +0200

    Improving dev script

--- dev.sh
+++ dev.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+set -e
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${MY_DIR}
+
+${MY_DIR}/setup.sh
+
+if [[ ! -f ${MY_DIR}/.env ]] || [[ -z `cat ${MY_DIR}/.env | grep SECRET_KEY_BASE` ]]; then
+  printf "SECRET_KEY_BASE=`bundle exec rake secret`\n" >> ${MY_DIR}/.env
+fi
+
+echo "Installing updated dependencies..."
+bundle install
 bundle exec foreman start -f Procfile.dev
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${MY_DIR}
+
+if [[ -z `which ruby` ]]; then
+  echo "Please ensure ruby is installed and on your path and try again."
+  exit 1
+fi
+
+if [[ -z `which gem` ]]; then
+  echo "Please ensure rubygems is installed and on your path and try again."
+  exit 1
+fi
+
+if [[ -z `which bundle` ]]; then
+  echo "Installing bundler..."
+  gem install bundler -N &> /dev/null
+fi
+
+mkdir -p ${MY_DIR}/tmp &> /dev/null
+
+echo "Installing dependencies..."
+bundle &> /dev/null
